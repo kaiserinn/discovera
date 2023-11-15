396ebc7142866e911bf50c83137b1a375b98dea2
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-11-15 12:56:58 +0800

    anuin anunya

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -11,14 +11,19 @@ const ResultBox = ({
 }) => {
   if (result) {
     return (
-      <div className="mx-10 rounded-md bg-white p-4 shadow-md"> <div style={{ backgroundColor: !isError ? 'rgb(220 252 231)' : 'rgb(254 202 202)' }}
-          className="rounded-md p-4 text-xl shadow-md self-stretch"
+      <div className="mx-10 rounded-md bg-white p-4 shadow-md">
+        {' '}
+        <div
+          style={{
+            backgroundColor: !isError ? 'rgb(220 252 231)' : 'rgb(254 202 202)',
+          }}
+          className="self-stretch rounded-md p-4 text-xl shadow-md"
         >
           {result}
         </div>
-        <div className="pt-4">{example}</div>
+        <div className="pb-20 pt-5 text-4xl">{example}</div>
         {gimmick && (
-          <img src={`../../public/${gimmick}`} className="pt-4 mx-auto" />
+          <img src={`../../public/${gimmick}`} className="mx-auto pb-32 pt-5" />
         )}
       </div>
     );
