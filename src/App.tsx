d9081d9587ac91bd547cc7f2874da538303c49ae
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-11-15 16:07:42 +0800

    anuin anunya

--- src/App.tsx
+++ src/App.tsx
@@ -15,62 +15,37 @@ export default function App() {
   const [selectValue, setSelectValue] = useState('id_en');
 
   useEffect(() => {
-    rbtId.add(
-      'penyintas',
-      'survivor',
-      'bear grylls is a survivor',
-      'grylls.gif'
-    );
+    rbtId.add('penyintas', 'survivor', 'bear grylls is a survivor', 'grylls.gif');
     rbtId.add('miskin', 'poor', 'poor guy', 'broke.gif');
     rbtId.add('apel', 'apple', 'eating apple is good for you');
     rbtId.add('makan', 'eat', 'you need to eat at least three times a day');
     rbtId.add('buku', 'book', 'reading book once a while feels comforting');
     rbtId.add('jatuh', 'fall', 'that thing can easily fall into that hole');
-    rbtId.add(
-      'runtuh', 
-      'collapse', 
-      'that building collapse majestically', 
-      'collapse.gif');
-    rbtId.add(
-      'cokelat',
-      'chocolate',
-      'chocolate is actually good for your health'
-    );
+    rbtId.add('runtuh', 'collapse', 'that building collapse majestically', 'collapse.gif');
+    rbtId.add('cokelat', 'chocolate', 'chocolate is actually good for your health');
     rbtId.add('bakar', 'burn', 'let us burn this city', 'burn.gif');
     rbtId.add('kereta', 'train', 'train is a very old mode of transportation');
     rbtId.add('tumbuh', 'grow', 'an oak tree can grow up to 43 meters tall');
+    rbtId.add('kertas dinding', 'wallpaper', 'this room has a nice wallpaper', 'wallpaper.jpg');
 
     rbtEn.add('poor', 'miskin', 'aku miskin sekali', 'broke.gif');
     rbtEn.add('apple', 'apel', 'makan apel itu baik untuk kesehatanmu');
     rbtEn.add('eat', 'makan', 'kamu harus makan setidaknya tiga kali sehari');
     rbtEn.add('book', 'buku', 'membaca buku di pagi hari itu asik');
     rbtEn.add('survivor', 'penyintas', 'bear grylls adalah seorang penyintas', 'grylls.gif');
-    rbtEn.add(
-      'fall',
-      'jatuh',
-      'pria jantan itu jatuh bebas tanpa menggunakan parasut'
-    );
+    rbtEn.add('fall', 'jatuh', 'pria jantan itu jatuh bebas tanpa menggunakan parasut');
     rbtEn.add('collapse', 'runtuh', 'bangunan itu runtuh dengan sangat cantik', 'collapse.gif');
     rbtEn.add('chocolate', 'cokelat', 'cokelat berwarna cokelat dan enak');
     rbtEn.add('burn', 'bakar', 'ayo bakar kota ini', 'burn.gif');
-    rbtEn.add(
-      'train',
-      'kereta',
-      'naik kereta di Indonesia sangat menyenangkan'
-    );
-    rbtEn.add(
-      'grow',
-      'tumbuh',
-      'pohon oak bisa tumbuh sampai ketinggian 43 meter'
-    );
+    rbtEn.add('train', 'kereta', 'naik kereta di Indonesia sangat menyenangkan');
+    rbtEn.add('grow', 'tumbuh', 'pohon oak bisa tumbuh sampai ketinggian 43 meter');
+    rbtEn.add('wallpaper', 'kertas dinding', 'ruangan ini memiliki kertas gambar', 'wallpaper.jpg');
   }, [rbtId, rbtEn]);
 
   const searchClicked = () => {
     if (!inputValue) return;
     const nodeTerjemahan =
-      selectValue === 'id_en'
-        ? rbtId.find(inputValue.toLowerCase())
-        : rbtEn.find(inputValue.toLowerCase());
+      selectValue === 'id_en' ? rbtId.find(inputValue.toLowerCase()) : rbtEn.find(inputValue.toLowerCase());
     if (nodeTerjemahan) {
       setTerjemahan(nodeTerjemahan.getValue());
       setExample(nodeTerjemahan.getExample());
@@ -107,12 +82,7 @@ export default function App() {
         setSelectValue={setSelectValue}
       />
 
-      <ResultBox
-        result={terjemahan}
-        example={example}
-        gimmick={gimmick}
-        isError={isError}
-      />
+      <ResultBox result={terjemahan} example={example} gimmick={gimmick} isError={isError} />
     </div>
   );
 }
--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -11,18 +11,18 @@ const ResultBox = ({
 }) => {
   if (result) {
     return (
-    <div className="mx-10 rounded-md gap-4 bg-white p-4 shadow-md flex">
-      <div className="text-xl w-full" >
-        <div style={{ backgroundColor: !isError ? 'rgb(220 252 231)' : 'rgb(254 202 202)' }}
-        className="rounded-md p-6 font-bold text-5xl">{result}</div>
-        <div className="bg-white rounded-md pt-6 text-3xl">
-          {example}
+      <div className="mx-10 flex gap-4 rounded-md bg-white p-4 shadow-md">
+        <div className="w-full text-xl">
+          <div
+            style={{ backgroundColor: !isError ? 'rgb(220 252 231)' : 'rgb(254 202 202)' }}
+            className="rounded-md p-6 text-5xl font-bold"
+          >
+            {result}
+          </div>
+          <div className="rounded-md bg-white pt-6 text-3xl">{example}</div>
         </div>
+        {gimmick && <img src={`../../public/${gimmick}`} className="max-w-xl" alt="Gimmick" />}
       </div>
-      {gimmick && (
-        <img src={`../../public/${gimmick}`} alt="Gimmick" />
-      )}
-    </div>
     );
   } else {
     return;
