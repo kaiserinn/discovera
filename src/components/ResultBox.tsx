a7881268aaeadaff7a91de263e785884082a7ad6
Miyawn <tengku.rayhan26@gmail.com>
2023-11-15 14:02:18 +0800

    commit message

--- src/components/ResultBox.tsx
+++ src/components/ResultBox.tsx
@@ -25,14 +25,14 @@ const ResultBox = ({
     <div className="mx-10 rounded-md gap-4 bg-white p-4 shadow-md flex">
       <div className="text-xl w-full" >
         <div style={{ backgroundColor: !isError ? 'rgb(220 252 231)' : 'rgb(254 202 202)' }}  
-        className="rounded-md p-6 font-bold text-4xl">{result}</div>
-        <div className="bg-white rounded-md  p-4 text-xl  lg:mb-0">
+        className="rounded-md p-6 font-bold text-5xl">{result}</div>
+        <div className="bg-white rounded-md  pt-6 text-3xl">
           {example}
         </div>
       </div>
       <div>
         {gimmick && (
-          <img src={`../../public/${gimmick}`}alt="Gimmick" />
+          <img src={`../../public/${gimmick}`} alt="Gimmick" />
         )}
       </div>
     </div>
