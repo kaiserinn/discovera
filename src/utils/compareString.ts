8e952b9df042dcb7fe5a5b4ab38d9b799555e07d
Pahril <88573655+visoredkon@users.noreply.github.com>
2023-11-15 14:42:14 +0800

    Refactor compareString function to improve readability and accuracy.

--- src/utils/compareString.ts
+++ src/utils/compareString.ts
@@ -1,12 +1,14 @@
-export function compareString(str1: string, str2: string){
-    const longestStrLen = str1.length > str2.length ? str1.length : str2.length;
+export function compareString(str1: string, str2: string): number {
+  const lengthStr1 = str1.length;
+  const lengthStr2 = str2.length;
+  const longestStrLen = Math.max(lengthStr1, lengthStr2);
 
-    let res = 0;
-    for(let i = 0; i < longestStrLen; ++i){
-        const ci1 = str1.charCodeAt(i) || 0;
-        const ci2 = str2.charCodeAt(i) || 0;
-        res += (ci1 - ci2);
-    }
+  let differenceSum = 0;
+  for (let i = 0; i < longestStrLen; ++i) {
+    const charCodeStr1 = str1.charCodeAt(i) || 0;
+    const charCodeStr2 = str2.charCodeAt(i) || 0;
+    differenceSum += charCodeStr1 - charCodeStr2;
+  }
 
-    return res / (Math.abs(res) || 1);
+  return differenceSum / Math.max(Math.abs(differenceSum), 1);
 }
